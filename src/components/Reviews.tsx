6097a1195c111a0d17b3d591faac29ba6b8af712
Afsana Meem <afsumeem@gmail.com>
2023-09-22 21:54:11 +0600

    post review functionality updated

---
+++ src/components/Reviews.tsx
@@ -0,0 +1,74 @@
+import { useState, ChangeEvent, FormEvent } from "react";
+import {
+  useGetReviewQuery,
+  usePostReviewMutation,
+} from "../redux/features/books/booksApi";
+import { BiSend } from "react-icons/bi";
+
+interface IProps {
+  id: string;
+}
+
+export default function Reviews({ id }: IProps) {
+  const [inputValue, setInputValue] = useState<string>("");
+
+  const { data } = useGetReviewQuery(id, {
+    refetchOnMountOrArgChange: true,
+    pollingInterval: 30000,
+  });
+
+  const [postReview, { isLoading, isError, isSuccess }] =
+    usePostReviewMutation();
+  console.log(isLoading, isError, isSuccess);
+
+  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    const options = {
+      id: id,
+      data: { reviews: inputValue },
+    };
+    console.log(inputValue);
+    postReview(options);
+    setInputValue("");
+  };
+
+  const handleChange = (event: ChangeEvent<HTMLTextAreaElement>) => {
+    setInputValue(event.target.value);
+  };
+
+  return (
+    <div className="my-5 mx-8">
+      <form className=" form-control flex gap-5  mb-8" onSubmit={handleSubmit}>
+        <label className="label">
+          <span className="label-text text-xl font-bold uppercase">
+            Write your Review
+          </span>
+        </label>
+        <div className="flex items-center">
+          <textarea
+            className="min-h-[30px] w-4/5 textarea textarea-bordered"
+            placeholder="Write here...."
+            onChange={handleChange}
+            value={inputValue}
+          />
+          <button
+            type="submit"
+            className="rounded-full h-10 w-10 p-2 text-[25px] text-blue-700"
+          >
+            <BiSend />
+          </button>
+        </div>
+      </form>
+      <hr />
+      <div className="my-10">
+        <h3 className="text-xl font-bold uppercase mb-8">All Reviews</h3>
+        {data?.reviews?.map((review: string, index: number) => (
+          <div key={index} className="flex gap-3 items-center mb-5">
+            <p className="italic"> - {review}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
--- src/pages/BookDetails.tsx
+++ src/pages/BookDetails.tsx
@@ -7,6 +7,7 @@ import {
   BiBookReader,
   BiEditAlt,
 } from "react-icons/bi";
+import Reviews from "../components/Reviews";
 
 export default function BookDetails() {
   const { id } = useParams();
@@ -15,39 +16,40 @@ export default function BookDetails() {
   console.log(book);
 
   return (
-    <div className="card lg:card-side bg-base-100 shadow-xl my-24 mx-8">
-      <figure>
-        <img src={book?.image} alt={book?.title} />
-      </figure>
-      <div className="card-body">
-        <h2 className="card-title uppercase text-2xl font-bold mb-8">
-          {book?.title}
-          <span className="badge lowercase badge-ghost">{book?.genre}</span>
-        </h2>
+    <>
+      <div className="card lg:card-side bg-base-100 shadow-xl my-24 mx-8 relative">
+        <figure>
+          <img src={book?.image} alt={book?.title} />
+        </figure>
+        <div className="card-body">
+          <h2 className="card-title uppercase text-2xl font-bold mb-2">
+            {book?.title}
+            <span className="badge lowercase badge-ghost">{book?.genre}</span>
+          </h2>
 
-        <h2>
-          by <span>{book?.author}</span>
-        </h2>
+          <h2 className="mb-8">
+            by <span>{book?.author}</span>
+          </h2>
 
-        <h4 className="">
-          Publication Date:
-          <span> {book?.publicationDate}</span>
-        </h4>
-        <div className="card-actions justify-end">
-          <button className="btn text-blue-950 hover:text-white font-bold bg-slate-400 hover:bg-blue-950 transition duration-1000">
-            <BiSolidHeart />
-          </button>
-          <button className="btn text-blue-950 hover:text-white font-bold bg-slate-300 hover:bg-blue-800 transition duration-1000">
-            <BiBookReader />
-          </button>
-          <button className="btn text-green-950 hover:text-white font-bold bg-green-400 hover:bg-green-800 transition duration-1000">
-            <BiEditAlt />
-          </button>
-          <button className="btn text-red-800 hover:text-white font-bold bg-red-300 hover:bg-red-600 transition duration-1000">
-            <BiSolidTrash />
-          </button>
-        </div>
-        <div>
+          <h4 className="">
+            Publication Date:
+            <span> {book?.publicationDate}</span>
+          </h4>
+          <div className="card-actions justify-end absolute end-8 bottom-12">
+            <button className="btn text-blue-950 hover:text-white font-bold bg-slate-400 hover:bg-blue-950 transition duration-1000">
+              <BiSolidHeart />
+            </button>
+            <button className="btn text-blue-950 hover:text-white font-bold bg-slate-300 hover:bg-blue-800 transition duration-1000">
+              <BiBookReader />
+            </button>
+            <button className="btn text-green-950 hover:text-white font-bold bg-green-400 hover:bg-green-800 transition duration-1000">
+              <BiEditAlt />
+            </button>
+            <button className="btn text-red-800 hover:text-white font-bold bg-red-300 hover:bg-red-600 transition duration-1000">
+              <BiSolidTrash />
+            </button>
+          </div>
+          {/* <div>
           <h2 className="text-xl font-bold ">Reviews about this book!</h2>
           <hr />
           <ol className="space-y-1 text-lg">
@@ -55,8 +57,13 @@ export default function BookDetails() {
               <li key={review}> - {review}</li>
             ))}
           </ol>
+        </div> */}
         </div>
       </div>
-    </div>
+
+      {/* review  */}
+      <hr />
+      <Reviews id={id!} />
+    </>
   );
 }
